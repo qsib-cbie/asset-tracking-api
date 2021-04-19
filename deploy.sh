826eeefe7cf5ca275aad316151dbca5370cea74a
Han Heul Jo <hanheuljo@gmail.com>
2021-04-19 18:16:42 -0500

    feat: update deployment script order to deploy cloudformation first

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+# deploy cloudformation template
+
 # build docker image
 docker build -f Dockerfile.aws -t docker-test .
 
 # push docker image to ecr
-
-# deploy cloudformation template
