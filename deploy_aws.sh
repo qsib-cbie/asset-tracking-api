01e7911a8dc9f84fdb76560660a6d90c7b419b65
Han Heul Jo <hanheuljo@gmail.com>
2021-04-25 22:50:05 -0500

    feat: update template with necessary permissions, TODO: create docker image in linux/amd64 platform

--- deploy_aws.sh
+++ deploy_aws.sh
@@ -6,4 +6,5 @@ aws cloudformation deploy --template-file "template.yaml" --stack-name "qsib-ass
         DBSubnet1="subnet-04f483713655db1cd" \
         DBSubnet2="subnet-0a26aadb34614ab3b" \
         DBSubnet3="subnet-098551910b310ea2f" \
-        RDSAdminUser="AdminUser"
+        RDSAdminUser="AdminUser" \
+    --capabilities CAPABILITY_IAM
